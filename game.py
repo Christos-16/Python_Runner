feb224a7b09bd16bcb35561439bbe6289f9f0632
Christos-16 <82862161+Christos-16@users.noreply.github.com>
2022-06-06 22:24:48 +0300

    lava high

--- game.py
+++ game.py
@@ -298,7 +298,7 @@ class World():
 					platform = Platform(col_count * tile_size, row_count * tile_size, 0, 1)
 					platform_group.add(platform)
 				if tile == 6:
-					lava = Lava(col_count * tile_size, row_count * tile_size + (tile_size // 2))
+					lava = Lava(col_count * tile_size, row_count * tile_size + (tile_size // 1.5))
 					lava_group.add(lava)
 				if tile == 7:
 					coin = Coin(col_count * tile_size + (tile_size // 2), row_count * tile_size + (tile_size // 2))
