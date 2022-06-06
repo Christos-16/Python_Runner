4a43ed1fb5774c8992b18538062adfa1322efbf9
Christos-16 <82862161+Christos-16@users.noreply.github.com>
2022-06-06 22:53:13 +0300

    change the image

--- game.py
+++ game.py
@@ -382,7 +382,7 @@ class Coin(pygame.sprite.Sprite):
 class Exit(pygame.sprite.Sprite):
 	def __init__(self, x, y):
 		pygame.sprite.Sprite.__init__(self)
-		img = pygame.image.load('img/exit.png')
+		img = pygame.image.load('img/door.png')
 		self.image = pygame.transform.scale(img, (tile_size, int(tile_size * 1.5)))
 		self.rect = self.image.get_rect()
 		self.rect.x = x
